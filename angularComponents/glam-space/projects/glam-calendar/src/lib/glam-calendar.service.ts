5094e0e7ffa505ac183fe2e97fbba05fed29c435
ojkey <kerim.orazmuhammedov@gmail.com>
2025-04-20 12:05:29 +0200

    glam-calendar

--- angularComponents/glam-space/projects/glam-calendar/src/lib/glam-calendar.service.ts
+++ angularComponents/glam-space/projects/glam-calendar/src/lib/glam-calendar.service.ts
@@ -13,15 +13,66 @@ import { parseISO } from 'date-fns/parseISO';
 })
 export class GlamCalendarService {
 
-  public years: Option[] = [ {
-    label: '2024',
-    value: 2024,
-  } ];
+  public years: Option[] = [
+    {
+      label: '2024',
+      value: 2024,
+    }, {
+      label: '2023',
+      value: 2023,
+    }
+  ];
 
-  public months: Option[] = [ {
-    label: 'September',
-    value: 9,
-  } ];
+  public months: Option[] = [
+    {
+      label: 'January',
+      value: 1,
+    },
+    {
+      label: 'February',
+      value: 2,
+    },
+    {
+      label: 'March',
+      value: 3,
+    },
+    {
+      label: 'April',
+      value: 4,
+    },
+    {
+      label: 'May',
+      value: 5,
+    },
+    {
+      label: 'June',
+      value: 6,
+    },
+    {
+      label: 'July',
+      value: 7,
+    },
+    {
+      label: 'August',
+      value: 8,
+    },
+    {
+      label: 'September',
+      value: 9,
+    },
+    {
+      label: 'October',
+      value: 10,
+    },
+    {
+      label: 'November',
+      value: 11,
+    },
+    {
+      label: 'December',
+      value: 12,
+    },
+  ];
 
   public weekDays: Option[] = [
     {
--- angularComponents/glam-space/projects/glam-calendar/src/lib/glam-select/glam-select.component.ts
+++ angularComponents/glam-space/projects/glam-calendar/src/lib/glam-select/glam-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Option } from '../model/option';
 
 @Component({
@@ -14,7 +14,28 @@ export class GlamSelectComponent {
   @Input()
   value: any;
 
+  @Output()
+  openPanel = new EventEmitter<MouseEvent>();
+
+  @Output()
+  valueChanges = new EventEmitter<any>();
+
   protected get label(): string | undefined {
     return this.options?.find(option => option.value === this.value)?.label;
+  }
+
+  protected onOpenPanel(event: MouseEvent): void {
+    this.openPanel.emit(event);
+  }
+
+  protected onMove(direction: number): void {
+    const index = this.options?.findIndex(option => option.value === this.value) || 0;
+    let newIndex = index + direction;
+    if (newIndex < 0) {
+      newIndex = this.options ? this.options.length - 1 : 0;
+    } else if (newIndex >= (this.options?.length || 0)) {
+      newIndex = ((this.options?.length || 2) - 1) - 1;
+    }
+    this.value = this.options ? [ newIndex ] || null;
   }
 }
